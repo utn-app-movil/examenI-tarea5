d8d1a6d0bc03b644601e26b23483d0a5c89f3573
Justin <justinvargas4299@gmail.com>
2024-11-09 18:26:29 -0600

    Examen1 y Tarea5

---
+++ app/src/main/java/identities/bib_Reservation.kt
@@ -0,0 +1,28 @@
+package cr.ac.utn.appmovil.identities
+
+import identities.Identifier
+import java.util.Date
+
+class bib_Reservation(
+    id: String = java.util.UUID.randomUUID().toString(),
+    studentName: String,
+    bookCode: String,
+    bookName: String,
+    reservationDate: Date,
+    returnDate: Date,
+    libraryLocation: String,
+    photoUri: String
+) : Identifier() {
+
+    override var Id: String = id
+    var StudentName: String = studentName
+    var BookCode: String = bookCode
+    var BookName: String = bookName
+    var ReservationDate: Date = reservationDate
+    var ReturnDate: Date = returnDate
+    var LibraryLocation: String = libraryLocation
+    var PhotoUri: String = photoUri
+
+    override val FullDescription: String
+        get() = "$StudentName reserved $BookName on $ReservationDate"
+}
